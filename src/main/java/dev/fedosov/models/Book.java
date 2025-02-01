dcf3a2d02ab7f6be273c887568efcd5948936c9e
m-fedosov <fedosovmichael@gmail.com>
2025-02-01 14:36:40 +0300

    feat: Person, Book entity

--- src/main/java/dev/fedosov/models/Book.java
+++ src/main/java/dev/fedosov/models/Book.java
@@ -1,5 +1,6 @@
 package dev.fedosov.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -7,19 +8,27 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Book {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotEmpty
+    @Column(name = "title")
     @Size(min = 2, max = 100, message = "Название должно быть от 2 до 100 символов")
     private String title;
 
     @NotEmpty
+    @Column(name = "author")
     @Size(min = 2, max = 100, message = "Автор должен быть от 2 до 100 символов")
     private String author;
 
+    @Column(name = "year")
     private int year;
 
+    @Column(name = "reserved_by")
     private Integer reservedBy;
 
     public Book() {}
