6fcc231e31745015aed7cd18f14ef44a69e68206
m-fedosov <fedosovmichael@gmail.com>
2025-02-01 14:44:01 +0300

    feat: People, Book repository

---
+++ src/main/java/dev/fedosov/repositories/BooksRepository.java
@@ -0,0 +1,9 @@
+package dev.fedosov.repositories;
+
+import dev.fedosov.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+}
---
+++ src/main/java/dev/fedosov/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package dev.fedosov.repositories;
+
+import dev.fedosov.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+}
