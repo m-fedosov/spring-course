80d3ded6c33b48109be92c56ccca1d8ee1fd4e06
m-fedosov <fedosovmichael@gmail.com>
2025-02-21 23:26:09 +0300

    feat: measurements table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO sensor(name, created_at, created_by) VALUES ('testSensor', now(), 'ADMIN');
+INSERT INTO measurements(value, raining, measure_time, measure_by) VALUES (21, false, now(), (SELECT id FROM sensor WHERE name = 'testSensor'));
+
