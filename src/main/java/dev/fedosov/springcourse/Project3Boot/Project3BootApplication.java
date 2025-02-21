6ee90e5fb554011569aec2596c9772bf5f17d55f
m-fedosov <fedosovmichael@gmail.com>
2025-02-21 20:02:11 +0300

    feat: project3 init

--- src/main/java/dev/fedosov/springcourse/Project3Boot/Project3BootApplication.java
+++ src/main/java/dev/fedosov/springcourse/Project3Boot/Project3BootApplication.java
@@ -1,13 +1,13 @@
-package dev.fedosov.springcourse.Project2Boot;
+package dev.fedosov.springcourse.Project3Boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project2BootApplication {
+public class Project3BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project2BootApplication.class, args);
+		SpringApplication.run(Project3BootApplication.class, args);
 	}
 
 }
